ddee7f3d3a7b4661b70e5ba04b5fea78dbb7a7f8
Absolutia <67517471+Absolutia@users.noreply.github.com>
2022-02-10 05:49:16 -0500

    clean up main.c

--- skywave-cli/main.c
+++ skywave-cli/main.c
@@ -25,14 +25,8 @@ void clearinpbuf(){
     return;
 }
 
-void* skwinit(){
-    if(t_init == true){
-    //uh, why does this still exist?
-    }
-    t_init = false;
-}
-
 void configuration_autoload(){
+    //temporary stub
     return;
 }
 
@@ -43,27 +37,10 @@ void* exitprompt(){
     exit(0);
 }
 
-void* inet(){
-    //when ready to attempt connection this function should run on a new thread
-
-    //check contarget
-    //attmpt handshake with target
-    //attmpt to conn if protocol supported
-    /** obviously not yet implemented */
-}
-
 int main(int argc, char *argv[])
 {
     system("clear");
     curses_init();
-    /*if(expui == true){
-        pthread_t cursesui;
-        pthread_create(&cursesui, NULL, EXPERIMENTAL_init, NULL);
-        pthread_join(cursesui, NULL);
-    }else{
-        pthread_t cursesui;
-        pthread_create(&cursesui, NULL, curses_init, NULL);
-        pthread_join(cursesui, NULL);
-    }*/
-    /*exitprompt(); //after all threads have merged again with the main thread, call the exit splash*/
+    /*Got rid of threading code here,
+    new threads will be spawned as needed from the curses session.*/
 }
